95145f1e38040f06ebb2bbf7bea7053af61a046f
Jesús A. Álvarez <zydeco@namedfork.net>
2019-01-26 21:01:47 +0100

    move state and buttons to SteamControllerInput.h

--- SteamController/SteamController.m
+++ SteamController/SteamController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SteamController.h"
+#import "SteamControllerInput.h"
 #import "SteamControllerExtendedGamepad.h"
 @import CoreBluetooth;
 
@@ -39,29 +40,6 @@ static void UpdateStatePad(GCExtendedGamepadSnapShotDataV100* state, SteamContro
     }
 }
 
-#define BUTTON_BACK 0x001000
-#define BUTTON_STEAM 0x002000
-#define BUTTON_FORWARD 0x004000
-#define BUTTON_LEFT_BUMPER 0x080000
-#define BUTTON_LEFT_TRIGGER 0x020000
-#define BUTTON_LEFT_GRIP 0x008000
-#define BUTTON_LEFT_TRACKPAD_CLICK 0x000002
-#define BUTTON_LEFT_TRACKPAD_CLICK_DOWN 0x000800
-#define BUTTON_LEFT_TRACKPAD_CLICK_LEFT 0x000400
-#define BUTTON_LEFT_TRACKPAD_CLICK_RIGHT 0x000200
-#define BUTTON_LEFT_TRACKPAD_CLICK_UP 0x000100
-#define BUTTON_LEFT_TRACKPAD_TOUCH 0x000008
-#define BUTTON_STICK 0x000040
-#define BUTTON_RIGHT_BUMPER 0x040000
-#define BUTTON_RIGHT_TRIGGER 0x010000
-#define BUTTON_RIGHT_GRIP 0x000001
-#define BUTTON_RIGHT_TRACKPAD_CLICK 0x000004
-#define BUTTON_RIGHT_TRACKPAD_TOUCH 0x000010
-#define BUTTON_A 0x800000
-#define BUTTON_B 0x200000
-#define BUTTON_X 0x400000
-#define BUTTON_Y 0x100000
-
 static CBUUID *SteamControllerInputCharacteristicUUID;
 static CBUUID *SteamControllerInputDescriptorUUID;
 static CBUUID *SteamControllerReportCharacteristicUUID;
@@ -69,16 +47,6 @@ static CBUUID *SteamControllerReportCharacteristicUUID;
 @interface SteamController () <CBPeripheralDelegate>
 - (void)playTune:(uint8_t)tune;
 @end
-
-typedef struct SteamPadState {
-    int16_t x, y;
-} SteamPadState;
-
-typedef struct SteamControllerState {
-    uint32_t buttons;
-    SteamPadState leftPad, rightPad, stick;
-    uint8_t leftTrigger, rightTrigger;
-} SteamControllerState;
 
 @implementation SteamController
 {
