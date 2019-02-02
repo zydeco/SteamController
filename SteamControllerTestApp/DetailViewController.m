cd2bbd1eec0ad28f597b00a2cae10491520dcb52
Jesús A. Álvarez <zydeco@namedfork.net>
2019-02-02 23:24:49 +0100

    update settings UI when values change

--- SteamControllerTestApp/DetailViewController.m
+++ SteamControllerTestApp/DetailViewController.m
@@ -22,20 +22,40 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     _controllerCell.controller = _steamController;
+    [_steamController addObserver:self forKeyPath:@"steamLeftTrackpadMapping" options:0 context:NULL];
+    [_steamController addObserver:self forKeyPath:@"steamRightTrackpadMapping" options:0 context:NULL];
+    [_steamController addObserver:self forKeyPath:@"steamThumbstickMapping" options:0 context:NULL];
+    [_steamController addObserver:self forKeyPath:@"steamLeftTrackpadRequiresClick" options:0 context:NULL];
+    [_steamController addObserver:self forKeyPath:@"steamRightTrackpadRequiresClick" options:0 context:NULL];
+    [self updateSettingsDisplay];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [_steamController removeObserver:self forKeyPath:@"steamLeftTrackpadMapping"];
+    [_steamController removeObserver:self forKeyPath:@"steamRightTrackpadMapping"];
+    [_steamController removeObserver:self forKeyPath:@"steamThumbstickMapping"];
+    [_steamController removeObserver:self forKeyPath:@"steamLeftTrackpadRequiresClick"];
+    [_steamController removeObserver:self forKeyPath:@"steamRightTrackpadRequiresClick"];
+    _controllerCell.controller = nil;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if (object == _steamController) {
+        [self updateSettingsDisplay];
+    }
+}
+
+- (IBAction)identifyController:(id)sender {
+    [self.steamController identify];
+}
+
+- (void)updateSettingsDisplay {
     _leftTrackpadMapping.selectedSegmentIndex = _steamController.steamLeftTrackpadMapping;
     _leftTrackpadRequiresClick.on = _steamController.steamLeftTrackpadRequiresClick;
     _rightTrackpadMapping.selectedSegmentIndex = _steamController.steamRightTrackpadMapping;
     _rightTrackpadRequiresClick.on = _steamController.steamRightTrackpadRequiresClick;
     _stickMapping.selectedSegmentIndex = _steamController.steamThumbstickMapping;
-}
-
-- (void)viewDidDisappear:(BOOL)animated {
-    [super viewDidDisappear:animated];
-    _controllerCell.controller = nil;
-}
-
-- (IBAction)identifyController:(id)sender {
-    [self.steamController identify];
 }
 
 - (IBAction)updateSetting:(id)sender {
