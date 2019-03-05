7d2451487029f2f6dde3193d7ecd3ef061003360
Jesús A. Álvarez <zydeco@namedfork.net>
2019-03-05 10:03:13 +0100

    exclude IOKit if STEAMCONTROLLER_NO_IOKIT is defined

--- SteamController/SteamControllerManager.h
+++ SteamController/SteamControllerManager.h
@@ -17,11 +17,23 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly) NSArray<SteamController*> *controllers;
 
 + (instancetype)sharedManager;
-+ (BOOL)listenForConnections;
 
 /** Scans for steam controllers in bluetooth mode */
 - (void)scanForControllers;
 
 @end
+
+#ifndef STEAMCONTROLLER_NO_IOKIT
+@interface SteamControllerManager (IOKit)
+
+/** Listens for controller connections.
+ 
+ This enables controllers to be detected automatically when they connect/reconnect, without calling `scanForControllers`.
+ This feature calls IOKit functions dynamically, which is private API on iOS/tvOS, it can be excluded from the build by
+ passing `-DSTEAMCONTROLLER_NO_IOKIT` to the compiler.
+ */
++ (BOOL)listenForConnections;
+@end
+#endif
 
 NS_ASSUME_NONNULL_END
